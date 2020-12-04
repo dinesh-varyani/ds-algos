c6578fd4daf0bc33ca2104bde83e3fa6fee5ac25
Dinesh <java.hubberspot@gmail.com>
2020-12-04 13:43:14 +0530

    Adding Array Implementations

---
+++ src/com/hubberspot/dsalgo/array/ArrayUtil.java
@@ -0,0 +1,79 @@
+package com.hubberspot.dsalgo.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayUtil {
+
+    public void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public int findMinimum(int[] arr) {
+        // edge case
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        int min = arr[0]; // min will hold the minimum of array
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public void reverse(int[] numbers, int start, int end) {
+        while (start < end) {
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (!map.containsKey(target - numbers[i])) {
+                map.put(numbers[i], i);
+            } else {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+        }
+        throw new IllegalArgumentException("Two numbers not found");
+    }
+
+    public void arrayDemo() {
+        int[] myArray = new int[5]; // default values // 0 1 2 3 4
+        // printArray(myArray);
+        myArray[0] = 5;
+        myArray[1] = 1;
+        myArray[2] = 8;
+        myArray[3] = 2;
+        myArray[4] = 10;
+        myArray[2] = 9;
+        printArray(myArray);
+        System.out.println(myArray.length);
+        System.out.println(myArray[myArray.length - 1]);
+
+        int[] arr = {5, 1, 8, 10};
+        printArray(arr);
+    }
+
+    public static void main(String[] args) {
+        ArrayUtil arrUtil = new ArrayUtil();
+        arrUtil.arrayDemo();
+    }
+
+}
